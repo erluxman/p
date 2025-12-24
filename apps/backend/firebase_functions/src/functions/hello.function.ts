400cb6d918fbb3306493394a66676a7e7c17c062
erluxman <erluxman@gmail.com>
2025-12-24 12:46:36 +0545

    added comments

--- apps/backend/firebase_functions/src/functions/hello.function.ts
+++ apps/backend/firebase_functions/src/functions/hello.function.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import * as functions from 'firebase-functions';
+//test comment
 
 export const helloWorld = async (req: Request, res: Response): Promise<void> => {
   try {
